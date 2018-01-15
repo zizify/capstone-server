3fd036dc41ee975de31529cf308c5b9abe70ed9b
Andy Gaines <andyagaines@gmail.com>
2018-01-15 15:55:53 -0600

    Removes console.log

--- assignments/router.js
+++ assignments/router.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+// const jsonParser = bodyParser.json();
 const router = express.Router();
 const {Assignment} = require('./models');
 
@@ -10,7 +10,6 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 
 //Create a new assignment
 router.post('/teacher', jwtAuth, (req, res) => {
-    console.log(req.user);
 	if (!req.user.isTeacher) return res.status(403).json({code: 403, message: 'Nice try, but no dice.'});
     
 	const {title, subject, teacher, className, points, goals, instructions, assignDate, dueDate, students} = req.body;
