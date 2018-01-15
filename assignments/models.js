dead9b718acab214a607135d97a13654cce3fb88
Andy Gaines <andyagaines@gmail.com>
2018-01-15 15:22:57 -0600

    creates first draft of assignment router post

--- assignments/models.js
+++ assignments/models.js
@@ -3,35 +3,35 @@ const mongoose = require('mongoose');
 
 const AssignmentSchema = mongoose.Schema({
 	title: {type: String, required: true},
-  subject: {type: String, required: true},
-  teacher: {type: String, required: true},
-  className: {type: String, required: true},
-  points: {type: Number, required: true},
-  goals: {type: String, required: true},
-  instructions: {type: String, required: true},
-  assignDate: {type: Date, required: true, default: Date.now },
-  dueDate: {type: Date, required: true},
-  students: {type: Array}
+	subject: {type: String, required: true},
+	teacher: {type: String, required: true},
+	className: {type: String, required: true},
+	points: {type: Number, required: true},
+	goals: {type: String, required: true},
+	instructions: {type: String, required: true},
+	assignDate: {type: Date, required: true, default: Date.now },
+	dueDate: {type: Date, required: true},
+	students: {type: Array}
 });
 
 AssignmentSchema.methods.serialize = function () {
-    const {
-      title,
-      subject,
-      teacher,
-      className,
-      points,
-      goals,
-      instructions,
-      assignDate,
-      dueDate,
-      students
-    } = this;
-    return { title, subject, teacher, className, points, goals, instructions, assignDate, dueDate, students };
+	const {
+		title,
+		subject,
+		teacher,
+		className,
+		points,
+		goals,
+		instructions,
+		assignDate,
+		dueDate,
+		students
+	} = this;
+	return { title, subject, teacher, className, points, goals, instructions, assignDate, dueDate, students };
 };
 
 const Assignment = mongoose.model('Assignment', AssignmentSchema);
 
 module.exports = {
-  Assignment,
+	Assignment,
 };
---
+++ assignments/router.js
@@ -0,0 +1,24 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const router = express.Router();
+const {Assignment} = require('./models');
+
+//Create a new assignment
+router.post('/teacher', jsonParser, (req, res) => {
+	const {title, subject, teacher, className, points, goals, instructions, assignDate, dueDate, students} = req.body;
+	Assignment.create({
+		title,
+		subject,
+		teacher,
+		className,
+		points,
+		goals,
+		instructions,
+		assignDate,
+		dueDate,
+		students
+	}).then(assignment => res.status(201).json(assignment.serialize()))
+		.catch(err => res.status(500).json({code: 500, message: 'We done fucked up.'}));
+});
