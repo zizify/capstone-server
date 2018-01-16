81c26bcc9a7d42ef89cdc38f621e4e8bac14fc09
Andy Gaines <andyagaines@gmail.com>
2018-01-16 11:26:39 -0600

    Adds put and post methods to assignments router

--- assignments/router.js
+++ assignments/router.js
@@ -28,4 +28,44 @@ router.post('/teacher', jwtAuth, (req, res) => {
 		.catch(err => res.status(500).json({code: 500, message: 'Bad request.', err}));
 });
 
+//Modify an existing assignment to change properties other than students.
+router.put('/teacher/change/:id', jwtAuth, (req, res) => {
+	const toUpdate = {};
+	const updateableFields = ['title', 'subject', 'className', 'points', 'goals', 'instructions', 'assignDate', 'dueDate'];
+    
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			toUpdate[field] = req.body[field];
+		}
+	});
+
+	Assignment
+		.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+		.then(newAssignment => res.status(204).json({newAssignment}))
+		.catch(err => res.status(500).json({message: 'Internal server error', err}));
+});
+
+//Add students to an assignment.
+router.post('/teacher/add/:id', jwtAuth, (req, res) => {
+	let newStudents;
+	if ('students' in req.body) {
+		newStudents = req.body.students;
+	} else {
+		res.status(500).json({message: 'Internal server error'});
+	}
+
+	Assignment
+		.findById(req.params.id)
+		.then(assignment => {
+			let newArray = assignment.students;
+			for (let i = 0; i < newStudents.length; i++) {
+				newArray.push(newStudents[i]);
+			}
+			return Assignment.findByIdAndUpdate(req.params.id, {students: assignment.students}, {new: true});
+		})
+		.then(newAssignment => res.status(201).json({newAssignment}))
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+
+});
+
 module.exports = {router};
