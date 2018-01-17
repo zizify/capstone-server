0f443ac3675fc01851c721745a41b26d60d8b507
Andy Gaines <andyagaines@gmail.com>
2018-01-17 15:02:08 -0600

    Starts method to add classes to teacher user

--- users/router.js
+++ users/router.js
@@ -1,148 +1,168 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const {User} = require('./models');
-
-const router = express.Router();
-
 const jsonParser = bodyParser.json();
+const passport = require('passport');
+const jwtAuth = passport.authenticate('jwt', { session: false });
+const router = express.Router();
+const {User} = require('./models');
 
 // Post to register a new user
 router.post('/', jsonParser, (req, res) => {
-  const requiredFields = ['username', 'password', 'isTeacher'];
-  const missingField = requiredFields.find(field => !(field in req.body));
+	const requiredFields = ['username', 'password', 'isTeacher'];
+	const missingField = requiredFields.find(field => !(field in req.body));
 
-  if (missingField) {
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: 'Missing field',
-      location: missingField
-    });
-  }
+	if (missingField) {
+		return res.status(422).json({
+			code: 422,
+			reason: 'ValidationError',
+			message: 'Missing field',
+			location: missingField
+		});
+	}
 
-  const stringFields = ['username', 'password', 'firstName', 'lastName'];
-  const nonStringField = stringFields.find(
-    field => field in req.body && typeof req.body[field] !== 'string'
-  );
+	const stringFields = ['username', 'password', 'firstName', 'lastName'];
+	const nonStringField = stringFields.find(
+		field => field in req.body && typeof req.body[field] !== 'string'
+	);
 
-  if (nonStringField) {
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: 'Incorrect field type: expected string',
-      location: nonStringField
-    });
-  }
+	if (nonStringField) {
+		return res.status(422).json({
+			code: 422,
+			reason: 'ValidationError',
+			message: 'Incorrect field type: expected string',
+			location: nonStringField
+		});
+	}
 
-  // If the username and password aren't trimmed we give an error.  Users might
-  // expect that these will work without trimming (i.e. they want the password
-  // "foobar ", including the space at the end).  We need to reject such values
-  // explicitly so the users know what's happening, rather than silently
-  // trimming them and expecting the user to understand.
-  // We'll silently trim the other fields, because they aren't credentials used
-  // to log in, so it's less of a problem.
-  const explicityTrimmedFields = ['username', 'password'];
-  const nonTrimmedField = explicityTrimmedFields.find(
-    field => req.body[field].trim() !== req.body[field]
-  );
+	// If the username and password aren't trimmed we give an error.  Users might
+	// expect that these will work without trimming (i.e. they want the password
+	// "foobar ", including the space at the end).  We need to reject such values
+	// explicitly so the users know what's happening, rather than silently
+	// trimming them and expecting the user to understand.
+	// We'll silently trim the other fields, because they aren't credentials used
+	// to log in, so it's less of a problem.
+	const explicityTrimmedFields = ['username', 'password'];
+	const nonTrimmedField = explicityTrimmedFields.find(
+		field => req.body[field].trim() !== req.body[field]
+	);
 
-  if (nonTrimmedField) {
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: 'Cannot start or end with whitespace',
-      location: nonTrimmedField
-    });
-  }
+	if (nonTrimmedField) {
+		return res.status(422).json({
+			code: 422,
+			reason: 'ValidationError',
+			message: 'Cannot start or end with whitespace',
+			location: nonTrimmedField
+		});
+	}
 
-  const sizedFields = {
-    username: {
-      min: 1
-    },
-    password: {
-      min: 3,
-      // bcrypt truncates after 72 characters, so let's not give the illusion
-      // of security by storing extra (unused) info
-      max: 72
-    }
-  };
-  const tooSmallField = Object.keys(sizedFields).find(
-    field =>
-      'min' in sizedFields[field] &&
+	const sizedFields = {
+		username: {
+			min: 1
+		},
+		password: {
+			min: 3,
+			// bcrypt truncates after 72 characters, so let's not give the illusion
+			// of security by storing extra (unused) info
+			max: 72
+		}
+	};
+	const tooSmallField = Object.keys(sizedFields).find(
+		field =>
+			'min' in sizedFields[field] &&
             req.body[field].trim().length < sizedFields[field].min
-  );
-  const tooLargeField = Object.keys(sizedFields).find(
-    field =>
-      'max' in sizedFields[field] &&
+	);
+	const tooLargeField = Object.keys(sizedFields).find(
+		field =>
+			'max' in sizedFields[field] &&
             req.body[field].trim().length > sizedFields[field].max
-  );
+	);
 
-  if (tooSmallField || tooLargeField) {
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: tooSmallField
-        ? `Must be at least ${sizedFields[tooSmallField]
-          .min} characters long`
-        : `Must be at most ${sizedFields[tooLargeField]
-          .max} characters long`,
-      location: tooSmallField || tooLargeField
-    });
-  }
+	if (tooSmallField || tooLargeField) {
+		return res.status(422).json({
+			code: 422,
+			reason: 'ValidationError',
+			message: tooSmallField
+				? `Must be at least ${sizedFields[tooSmallField]
+					.min} characters long`
+				: `Must be at most ${sizedFields[tooLargeField]
+					.max} characters long`,
+			location: tooSmallField || tooLargeField
+		});
+	}
 
-  let {username, password, firstName = '', lastName = '', isTeacher = false} = req.body;
-  // Username and password come in pre-trimmed, otherwise we throw an error
-  // before this
-  firstName = firstName.trim();
-  lastName = lastName.trim();
+	let {username, password, firstName = '', lastName = '', isTeacher = false} = req.body;
+	// Username and password come in pre-trimmed, otherwise we throw an error
+	// before this
+	firstName = firstName.trim();
+	lastName = lastName.trim();
 
-  return User.find({username})
-    .count()
-    .then(count => {
-      if (count > 0) {
-        // There is an existing user with the same username
-        return Promise.reject({
-          code: 422,
-          reason: 'ValidationError',
-          message: 'Username already taken',
-          location: 'username'
-        });
-      }
-      // If there is no existing user, hash the password
-      return User.hashPassword(password);
-    })
-    .then(hash => {
-      return User.create({
-        username,
-        password: hash,
-        firstName,
-        lastName,
-        isTeacher
-      });
-    })
-    .then(user => {
-      return res.status(201).json(user.serialize());
-    })
-    .catch(err => {
-      // Forward validation errors on to the client, otherwise give a 500
-      // error because something unexpected has happened
-      if (err.reason === 'ValidationError') {
-        return res.status(err.code).json(err);
-      }
-      res.status(500).json({code: 500, message: 'Internal server error'});
-    });
+	return User.find({username})
+		.count()
+		.then(count => {
+			if (count > 0) {
+				// There is an existing user with the same username
+				return Promise.reject({
+					code: 422,
+					reason: 'ValidationError',
+					message: 'Username already taken',
+					location: 'username'
+				});
+			}
+			// If there is no existing user, hash the password
+			return User.hashPassword(password);
+		})
+		.then(hash => {
+			return User.create({
+				username,
+				password: hash,
+				firstName,
+				lastName,
+				isTeacher
+			});
+		})
+		.then(user => {
+			return res.status(201).json(user.serialize());
+		})
+		.catch(err => {
+			// Forward validation errors on to the client, otherwise give a 500
+			// error because something unexpected has happened
+			if (err.reason === 'ValidationError') {
+				return res.status(err.code).json(err);
+			}
+			res.status(500).json({code: 500, message: 'Internal server error'});
+		});
 });
 
 // Never expose all your users like below in a prod application
 // we're just doing this so we have a quick way to see
 // if we're creating users. keep in mind, you can also
 // verify this in the Mongo shell.
-router.get('/', (req, res) => {
-  return User.find()
-    .then(users => res.json(users.map(user => user.serialize())))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+// router.get('/', (req, res) => {
+// 	return User.find()
+// 		.then(users => res.json(users.map(user => user.serialize())))
+// 		.catch(err => res.status(500).json({message: 'Internal server error'}));
+// });
+
+router.post('/class/create', jwtAuth, (req, res) => {
+	if (req.user.isTeacher === false) {
+		Promise.reject({message: 'Student users cannot create classes.'});
+	}
+  
+	const {className, userIds} = req.body;
+  
+	User
+		.find({'username': { $in: userIds}})
+		.then(users => users.filter(each => !each.isTeacher))
+		.then(students => User
+			.findOne({username: req.user.username})
+			.then(user => {
+				user.classes.push({className, userIds});
+				user.save();
+				// return User.findByIdAndUpdate(req.user._id, {classes: user.classes.push({className, students})}, {new: true});
+			}))
+		.then(() => res.status(201).json())
+		.catch(err => res.status(500).json({message: 'Internal server error.', err: err}));
 });
 
 module.exports = {router};
